0b417202729f5d97679d4b826ec0eeb6f26880f6
sentokun <p53-tb-tashiro@live.jp>
2015-09-22 02:11:49 +0000

    challenge2 report

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     # 内容は必須入力かつ2文字以上30文字以内
     validates :body , length: { minimum: 2 , maximum: 30 } , presence:true
+    # 
+    validates_numericality_of :age , greater_than: 0, only_integer:true , presence:true
 end
